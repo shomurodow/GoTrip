f93a8fb422f1100aa40e71c5c5ec9d797c4ed78f
shomurodovX <cometa11we@gmail.com>
2024-03-07 15:31:42 +0500

    The Destinations section is ready.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log(0.1 + 0.2);
+const owl = $(".owl-carousel");
+owl.owlCarousel({
+  center: true,
+  loop: true,
+  margin: 30,
+  startPosition: 1,
+  items: 3,
+});
+$(".slider__btn--prev").click(function () {
+  owl.trigger("prev.owl.carousel");
+});
+$(".slider__btn--next").click(function () {
+  owl.trigger("next.owl.carousel");
+});
